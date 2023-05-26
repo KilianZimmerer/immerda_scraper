ded1ec2b71c9a26c3a1b2988c35551dd255e148a
kilian <21954712+KilianZimmerer@users.noreply.github.com>
2023-05-26 20:40:16 +0200

    include TRY HARD shifts

--- immerda_scraper/scraper.py
+++ immerda_scraper/scraper.py
@@ -8,12 +8,21 @@ page = requests.get(url)
 
 soup = BeautifulSoup(page.text, 'html.parser')
 names = soup.find_all("i")
-try_hard = soup.find_all("")
+try_hards = []
+tds = soup.find_all("td", {"class": "text-sm max-w-xs px-6 py-4"})
+for td in tds:
+    try_hards.append(td.text)
 
 all_names = []
-for name in names:
+all_try_hards = []
+for index, name in enumerate(names):
+    if name.text == '\n':
+        continue
     for nam in name.string.strip().split(","):
         all_names.append(nam.strip())
+        all_try_hards.append(try_hards[index])
 
-df = pd.DataFrame({"names": all_names})["names"].str.lower()
-df.value_counts().sort_index().to_csv("counted_shifts.csv")
+df = pd.DataFrame({"names": all_names, "try_hards": all_try_hards})
+df["names"] = df["names"].str.lower()
+df.loc[df['try_hards'] == "TRY HARD", 'names'] += " (try hard)"
+df["names"].value_counts().sort_index().to_csv("counted_shifts.csv")
